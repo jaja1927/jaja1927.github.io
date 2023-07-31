e4a9b00985cd119d2c3a94d91f99e525b3a5cd74
unknown <softech.series@gmail.com>
2023-07-31 09:35:55 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,18 @@ module.exports = {
   darkMode: "class",
   content: ["index.html"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#06b6d4",
         abu2: "#6b7280",
         darkMode: ["class", '[data-mode="dark"]'],
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
